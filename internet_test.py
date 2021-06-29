c3262b5435779ddb54dce4a5ffc7a07cfbe29dac
Slater-C <82797465+Slater-C@users.noreply.github.com>
2021-06-29 18:43:18 -0400

    new feature update
    - continuous ping with variable delay (ensures 15 pings per minute)
    - 24 hour failed pings now updates on every failed ping
    - ping bar has two layers, preserving history for longer

--- internet_test.py
+++ internet_test.py
@@ -51,6 +51,42 @@ class pingHistory:
             failed_pings += self.day_failures[x]
         return failed_pings
 
+class pingBar:
+    def __init__(self, size):
+        self.size = size
+        self.ledBar = ['pending'] * size
+    
+    def init_pingBar(self):
+        for x in range(0, self.size):
+            unicornhatmini.set_pixel(x + 1, 6, 0, 0, 20) #x, y, r, g, b
+        unicornhatmini.show()
+
+    def store_ping(self, index, success):
+        if(success):
+            self.ledBar[index] = 'success'
+            unicornhatmini.set_pixel(index + 1, 6, 0, 200, 0) #x, y, r, g, b
+            unicornhatmini.show()
+        else:
+            self.ledBar[index] = 'failure'
+            unicornhatmini.set_pixel(index + 1, 6, 200, 0, 0) #x, y, r, g, b
+            unicornhatmini.show()
+    
+    def pend_ping(self, index):
+        unicornhatmini.set_pixel(index + 1, 6, 0, 0, 200) #x, y, r, g, b
+        unicornhatmini.show()
+                
+    def new_layer(self):
+        for x in range(0, self.size):
+            if(self.ledBar[x] == 'success'):
+                unicornhatmini.set_pixel(x + 1, 6, 0, 15, 0) #x, y, r, g, b
+            elif (self.ledBar[x] == 'failure'):
+                unicornhatmini.set_pixel(x + 1, 6, 15, 0, 0) #x, y, r, g, b
+            else:
+                unicornhatmini.set_pixel(x + 1, 6, 0, 0, 15) #x, y, r, g, b
+        unicornhatmini.show()
+
+            
+
 
 def append_file(ping_total, ping_successes, ping_failures, avg_rtt):
     
@@ -81,14 +117,14 @@ def text_draw(font, display_width, display_height, lost_pings):
 
     offset_x = display_width - (display_width - text_width)/2
 
-    print("text width is: " + str(text_width) + "  |   offset x is:" + str(offset_x))
+    # print("text width is: " + str(text_width) + "  |   offset x is:" + str(offset_x))
 
     for y in range(0, 6):
         for x in range(display_width):
             #hue = 160 #(time.time() / 10.0) + (x / float(display_width * 2))
             #r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
-            if image.getpixel((x + offset_x, y-1)) == 255:
-                unicornhatmini.set_pixel(x, y, 0, 0, 255)
+            if image.getpixel((x + offset_x, y)) == 255:
+                unicornhatmini.set_pixel(x, y, 255, 0, 20)
             else:
                 unicornhatmini.set_pixel(x, y, 0, 0, 0)
 
@@ -117,31 +153,32 @@ def main():
     # Granted it's actually 5x8 for some reason :| but that doesn't matter
     font = ImageFont.truetype("5x7.ttf", 8)
 
-    
 
-
-
-    wait = 0
-    wait2 = time.time() + 720
+    cycle_delay = 0
+    save_delay = time.time() + 720
     cycle = pingStats_cycle()
     day = pingHistory()
+    bar = pingBar(size = 15)
+
+    bar.init_pingBar()
     
     while(True):
         
         time.sleep(1)
         seconds = time.time()
         
-        if((wait + 60) < seconds):
+        if((cycle_delay + 60) < seconds):
             
-            wait = time.time()
+            cycle_delay = time.time()
             fails = 0
 
-            for i in range (0, 15):
-                unicornhatmini.set_pixel(i+1, 6, 0, 0, 0) #x, y, r, g, b
+            bar.new_layer();    # Move previous cycle of pings to a dim layer, start new layer on top
             
             for x in range (0, 15):
-                unicornhatmini.set_pixel(x+1, 6, 0, 0, 200) #x, y, r, g, b
-                unicornhatmini.show()
+                
+                ping_start = time.time() * 1000
+                
+                bar.pend_ping(index = x)
                 
                 while True:
                     try:
@@ -153,28 +190,37 @@ def main():
                 
                 fail = int(response_list.packet_loss)
                 fails += fail
-                if(fail):
-                    unicornhatmini.set_pixel(x+1, 6, 200, 0, 0) #x, y, r, g, b
-                if(not fail):
-                    unicornhatmini.set_pixel(x+1, 6, 0, 200, 0) #x, y, r, g, b
-                unicornhatmini.show()
+                
+                bar.store_ping(index = x, success = not fail)
 
                 cycle.ping_total += 1
                 cycle.ping_success += int(1 - response_list.packet_loss)
                 cycle.ping_failure += fail
                 cycle.append_rtt(response_list.rtt_avg_ms)
-                time.sleep(1)
+                
+                if(fail):
+                    text_draw(font, display_width, display_height, day.total_fails() + fails)
+
+                if (x != 14):
+                    while ((time.time() * 1000) < ping_start + 4000):
+                        pass
+
+
+                # time.sleep(1)   # PING FINISH
 
             day.queue_fails(fails)
 
             cycle.print_pingstats()
             print("\nFAILS IN 24 HOURS = " + str(day.total_fails()))
             
+            predraw = time.time() * 1000
             text_draw(font, display_width, display_height, day.total_fails())
+            postdraw = time.time() * 1000
+            print("time to draw is", postdraw - predraw)
             
 
-        if((wait2 + 600) < seconds):
-            wait2 = time.time()
+        if((save_delay + 600) < seconds):
+            save_delay = time.time()
             append_file(cycle.ping_total, cycle.ping_success, cycle.ping_failure, cycle.get_avg_rtt())
             cycle.reset_cycle()
 
